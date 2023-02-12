91eae4d659ff43dbc62835e0a1823dfb777968e5
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-02-11 21:02:39 -0600

    added default paths

--- src/lib/model/Rogue1Save.ts
+++ src/lib/model/Rogue1Save.ts
@@ -1,6 +1,8 @@
 import type { Reader } from "../utils/Reader";
 import type { SaveFile } from "./SaveFile";
 
+export const ROGUE_ONE_DEFAULT_PATH = "C:/Users/USER/Documents/SavedGames/RogueLegacy/RogueLegacyStorageContainer/AllPlayers/PROFILE";
+
 export class Rogue1Save implements SaveFile {
   constructor(reader: Reader) {
     
--- src/lib/model/Rogue2Save.ts
+++ src/lib/model/Rogue2Save.ts
@@ -1,6 +1,8 @@
 import type { Reader } from "../utils/Reader";
 import type { SaveFile } from "./SaveFile";
 
+export const ROGUE_TWO_DEFAULT_PATH = "C:/Users/USER/AppData/LocalLow/Cellar Door Games/Rogue Legacy 2/Saves/PLATFORM/GAME_ID/PROFILE";
+
 export class Rogue2Save implements SaveFile {
   constructor(reader: Reader) {
     
